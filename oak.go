90adaf9a12e037fa8b1f825957fc3902e1fc4353
Uriel Robles <urielrobles802@gmail.com>
2023-03-04 01:26:13 -0600

    func handlers

--- oak.go
+++ oak.go
@@ -1,23 +1,54 @@
 package oak
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 type Oak struct {
 	routes  []string
 	AppName string
 	server  *http.ServeMux
+	logger  *log.Logger
 }
 
 func New() *Oak {
 	return &Oak{
 		AppName: "Default",
 		server:  &http.ServeMux{},
+		logger:  log.New(os.Stdout, "Api: ", log.LstdFlags),
 	}
 }
 
-func (o *Oak) Get(path string, handlerFn http.HandlerFunc) {
+func (o *Oak) GET(path string, handlerFn http.HandlerFunc) {
+	o.server.Handle(path, handlerFn)
+}
+
+func (o *Oak) POST(path string, handlerFn http.HandlerFunc) {
+	o.server.Handle(path, handlerFn)
+}
+
+func (o *Oak) PUT(path string, handlerFn http.HandlerFunc) {
+	o.server.Handle(path, handlerFn)
+}
+
+func (o *Oak) DELETE(path string, handlerFn http.HandlerFunc) {
+	o.server.Handle(path, handlerFn)
+}
+
+func (o *Oak) HEAD(path string, handlerFn http.HandlerFunc) {
+	o.server.Handle(path, handlerFn)
+}
+
+func (o *Oak) PATCH(path string, handlerFn http.HandlerFunc) {
+	o.server.Handle(path, handlerFn)
+}
+
+func (o *Oak) OPTIONS(path string, handlerFn http.HandlerFunc) {
 	o.server.Handle(path, handlerFn)
 }
 
@@ -27,5 +58,19 @@ func (o *Oak) Run() {
 		Handler: o.server,
 	}
 
-	s.ListenAndServe()
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			o.logger.Fatal(err)
+		}
+	}()
+
+	sigChannel := make(chan os.Signal)
+	signal.Notify(sigChannel, os.Interrupt)
+	signal.Notify(sigChannel, os.Kill)
+	sig := <-sigChannel
+	o.logger.Println("Received terminate, graceful shutdown", sig)
+	timeoutContext, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	s.Shutdown(timeoutContext)
 }
